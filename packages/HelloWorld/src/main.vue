393b6b6c0430736153df7fb3c44f51b3671a6676
Dustin <dustindowning.main@gmail.com>
2019-11-07 14:09:31 -0600

    example structure

---
+++ packages/HelloWorld/src/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello-world">
+    {{ message }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    message: {
+      type: String,
+      default: 'Hello World',
+    },
+  },
+};
+</script>
+<style>
+  .hello-world {
+    font-size: 24px;
+  }
+</style>
