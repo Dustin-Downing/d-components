99d2763ed3edeffaacabfd6100bd26374e942e69
Dustin <dustindowning.main@gmail.com>
2019-11-08 12:36:27 -0600

    MVP test

---
+++ src/components/HelloWorld/index.js
@@ -0,0 +1,4 @@
+// Import the HelloWorld component from this folder and send it down to ./components/index.js
+import HelloWorld from './HelloWorld';
+
+export default HelloWorld;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// In this file we explicity export everything. This is just to be thorough
+// and explicit. This thorough exporting method can seem like a lot, but it
+// allows for simpler scaling when our library grows in size
+export { default as HelloWorld } from './HelloWorld';
